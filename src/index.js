d5ac59e283491cb7ad7b56fd5c616bde31066293
wangtao0101 <yuecjn@gmail.com>
2017-05-23 20:57:36 +0800

    clear code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
 import stripComments from './stripComment';
 
 export default stripComments;
-
-// // import { abc as ccc } from 'helpalias/api';
-// // fsdfasdf
-// import { comabc } , d from './component/com' //sfasdf;
-// import { a, b, c, comabc } from './component';
-// var a = \`asdfasdf\`;
-// /*dfasdf
-// asdfasdf
-// */
-
-// const program = `
-// //aaa \r\n
-// \`
-// safsdfasdf
-// \`
-// //asdfasdfasdf
-// `;
-
-// console.log(stripComments(program));
-
