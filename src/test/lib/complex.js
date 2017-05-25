3e8cbe52790b827b61a58b2d417c1abeb5f4e529
wangtao0101 <yuecjn@gmail.com>
2017-05-25 11:06:47 +0800

    add lib

---
+++ src/test/lib/complex.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+
+const a = 'aaaaaa';
+// i am a comment
+
+const regex = /IAM\'\`\\[aa]AREGEX/g; //I am a regex;
+
+/**
+ * abc
+ * @param {*} cc
+ */
+function abc(cc) {
+    return 'cccc';
+}
+
+const b = "cccccc"; //I am b
+
+const c = 'asdfadsfasdf
+
+/* abc
+dfg\r\ndd
+*/
+
+const d = "ddddddddd";
+
+//ddddddddd
+
